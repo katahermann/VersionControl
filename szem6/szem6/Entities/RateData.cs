931b6e6aa0acdaa583c6ce7f3f59e6e9acfbcb17
Katalin Hermann <katalin.hermann@stud.uni-corvinus.hu>
2020-10-17 17:17:37 +0200

    Entities folder and datagridview

---
+++ szem6/szem6/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szem6.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+        public string Currancy { get; set; }
+        public decimal Value { get; set; }
+    }
+}
--- szem6/szem6/Form1.cs
+++ szem6/szem6/Form1.cs
@@ -7,16 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using szem6.Entities;
 using szem6.MnbServiceReference;
 
 namespace szem6
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            
+            dataGridView1.DataSource = Rates;
         }
 
         public void GetExchangeRates() 
