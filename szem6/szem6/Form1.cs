5757f24e8b210261496d5f49aeb76ca968740582
Katalin Hermann <katalin.hermann@stud.uni-corvinus.hu>
2020-10-17 18:20:36 +0200

    Currencies binding list fail

--- szem6/szem6/Form1.cs
+++ szem6/szem6/Form1.cs
@@ -19,6 +19,8 @@ namespace szem6
         BindingList<RateData> Rates = new BindingList<RateData>();
         string result;
 
+        //BindingList<string> Currencies = new BindingList<string>();
+
         public Form1()
         {
             InitializeComponent();
