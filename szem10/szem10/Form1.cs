b9d2a97e7f88b60ab706b4156c1d971852cb075c
Katalin Hermann <katalin.hermann@stud.uni-corvinus.hu>
2020-11-28 18:51:30 +0100

    Trying out the game

--- szem10/szem10/Form1.cs
+++ szem10/szem10/Form1.cs
@@ -21,6 +21,9 @@ namespace szem10
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
