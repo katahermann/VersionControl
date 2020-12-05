c7d13f0e5fb8f5f82a53d00e7afb3922da305d53
Katalin Hermann <katalin.hermann@stud.uni-corvinus.hu>
2020-12-05 22:00:40 +0100

    Created Patient Class

---
+++ Covid19/Covid19/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19
+{
+    class Patient
+    {
+        public Patient(int id, bool nem, bool kulfold, bool erintkez, bool tunet, bool korlatoz, bool teszt)
+        {
+            this.ID = id;
+            this.Nem = nem;
+            this.Kulfold = kulfold;
+            this.Erintkez = erintkez;
+            this.Tunet = tunet;
+            this.Korlatoz = korlatoz;
+            this.Teszt = teszt;
+        }
+        public int ID { get; }
+        public bool Nem { get;}
+        public bool Kulfold { get; }
+        public bool Erintkez { get; }
+        public bool Tunet { get; }
+        public bool Korlatoz { get; }
+        public bool Teszt { get; }
+    }
+}
